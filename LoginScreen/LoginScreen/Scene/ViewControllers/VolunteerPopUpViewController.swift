27b2d499aa23d6d52df90fbe38005468cb2ba121
Ali Halawa <ahhalawa@ucdavis.edu>
2020-01-19 12:24:09 -0800

    yo

--- LoginScreen/LoginScreen/Scene/ViewControllers/VolunteerPopUpViewController.swift
+++ LoginScreen/LoginScreen/Scene/ViewControllers/VolunteerPopUpViewController.swift
@@ -24,7 +24,11 @@ class VolunteerPopUpViewController: UIViewController {
     
     @IBOutlet weak var Decline: UIButton!
     
-    @IBOutlet weak var Charity: UIImageView!
+    @IBOutlet weak var Charity: UIImageView! {
+        didSet {
+            self.Charity.tappable = true
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +42,16 @@ class VolunteerPopUpViewController: UIViewController {
         Accept.clipsToBounds = true
         Decline.layer.cornerRadius = 10
         Decline.clipsToBounds = true
+
         // Do any additional setup after loading the view.
+        
+        self.Charity.callback = {
+             // Some actions etc.
+           print("pressed image")
+            if let url = URL(string: "https://www.stjude.org/donate/pm.html?sc_dcm=254910857&sc_cid=kwp75473&source_code=IIQ190721002&ef_id=EAIaIQobChMIx7a8tb2Q5wIVah6tBh0C4g_eEAAYASAAEgLf1vD_BwE:G:s&s_kwcid=AL!4519!3!386681578030!e!!g!!st%20judes&gclid=EAIaIQobChMIx7a8tb2Q5wIVah6tBh0C4g_eEAAYASAAEgLf1vD_BwE") {
+                UIApplication.shared.open(url)
+            }
+        }
     }
     
 
@@ -64,4 +77,49 @@ class VolunteerPopUpViewController: UIViewController {
 
     
 
+}
+
+
+public typealias SimpleClosure = (() -> ())
+private var tappableKey : UInt8 = 0
+private var actionKey : UInt8 = 1
+
+extension UIImageView {
+    
+    @objc var callback: SimpleClosure {
+        get {
+            return objc_getAssociatedObject(self, &actionKey) as! SimpleClosure
+        }
+        set {
+            objc_setAssociatedObject(self, &actionKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    var gesture: UITapGestureRecognizer {
+        get {
+            return UITapGestureRecognizer(target: self, action: #selector(tapped))
+        }
+    }
+    
+    var tappable: Bool! {
+        get {
+            return objc_getAssociatedObject(self, &tappableKey) as? Bool
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &tappableKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            self.addTapGesture()
+        }
+    }
+
+    fileprivate func addTapGesture() {
+        if (self.tappable) {
+            self.gesture.numberOfTapsRequired = 1
+            self.isUserInteractionEnabled = true
+            self.addGestureRecognizer(gesture)
+        }
+    }
+
+    @objc private func tapped() {
+        callback()
+    }
 }
