ccde60f7afd3fb1e4c1cb68857cfae683c95dcb5
Ali Halawa <ahhalawa@ucdavis.edu>
2020-01-19 10:42:26 -0800

    nothing

---
+++ LoginScreen/LoginScreen/Scene/ViewControllers/VolunteerPopUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VolunteerPopUpViewController.swift
+//  LoginScreen
+//
+//  Created by Ali Halawa on 1/19/20.
+//  Copyright © 2020 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class VolunteerPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
